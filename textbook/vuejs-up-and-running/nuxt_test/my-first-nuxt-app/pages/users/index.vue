c7b03ddf8aa5fac9487b3cb95df4058d4681595b
garigari-kun <keisuke.cs@gmail.com>
2019-04-30 00:36:18 +0900

    2019-04-30

---
+++ textbook/vuejs-up-and-running/nuxt_test/my-first-nuxt-app/pages/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>
+      /users/index.vue
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+        this.$axios.$get('http://qiita.com/api/v2/items?quiery=tag:nuxt.js'),
+        true,
+        ' '
+      )
+    )
+  }
+}
+</script>
